d17914348fd77d9724dbacf2606fcd507a459ae2
钟建华 <zhongjh@phone580.com>
2019-01-22 14:17:13 +0800

    1

--- src/main/java/zjh/test/HelloWorldController.java
+++ src/main/java/zjh/test/HelloWorldController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @RequestMapping("/hello")
     public String index() {
-        return "Hello World";
+        return "Hello World1";
     }
 }
