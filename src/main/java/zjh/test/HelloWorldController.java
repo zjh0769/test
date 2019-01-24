98c4dcb1f2466567230e852985669571717f99f5
钟建华 <zhongjh@phone580.com>
2019-01-24 17:08:04 +0800

    1

--- src/main/java/zjh/test/HelloWorldController.java
+++ src/main/java/zjh/test/HelloWorldController.java
@@ -5,8 +5,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @RequestMapping("/hello")
-    public String index() {
-        return "Hello World1~";
-    }
+	@RequestMapping("/hello")
+	public String index() {
+		return "Hello World1~";
+	}
+
+	@RequestMapping("/call")
+	public void call() {
+		try {
+			int i = 10000;
+			while (i < 0) {
+				HttpRequest.sendGet("server2:8080/hello2", null);
+				i--;
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
